a42a019bf4737a3dc6c45512b3f0fc61d47ad07e
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-16 19:51:07 +0800

    Update PhysicsCoordinate.jl

--- PhysicsCoordinate.jl
+++ PhysicsCoordinate.jl
@@ -19,7 +19,14 @@ abstract type Coordinate <:AbstractVector{Real} end
                 return CC.z
             end
         end
+        function CartesianCoordinate(x::Real, y::Real, z::Real)
+            x₂ = convert(Float64, x)
+            y₂ = convert(Float64, y)
+            z₂ = convert(Float64, z)
+            return CartesianCoordinate(x₂, y₂, z₂)
+        end
 
 @testset "Functionality" begin
     @test isa(CartesianCoordinate(1.0, 2.0, 3.0), CartesianCoordinate)
+    @test isa(CartesianCoordinate(1, 2//1, 3.0), CartesianCoordinate)
 end
