b9dfdc44fd363739d1c9d22e0bea2ceb02a33c6e
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-16 23:52:00 +0800

    Update PhysicsScalar.jl

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -6,6 +6,10 @@ function (A::Unitful.FreeUnits)(m::Real)
 end
 
 abstract type PhysicsScalar <: Real end
+    function Base.:+(a::T, b::T) where {T<:PhysicsScalar}
+        c = a.magnitude + b.magnitude
+        return T(c)
+    end
 
     struct Speed <: PhysicsScalar
         magnitude::typeof(1.0u"m/s")
@@ -22,9 +26,10 @@ abstract type PhysicsScalar <: Real end
         end
     end
 
-@testset begin
+@testset "ScalarVectors Functionalities" begin
     @test 12.3u"m/s" == u"m/s"(12.3)
     @test Speed(12u"m/s") == Speed(12.0u"m/s")
     @test Speed(12u"mm/s") == Speed(12u"m/ks")
     @test Speed(12.3) == Speed(12.3u"m/s")
+    @test Speed(12.3) + Speed(44.4) == Speed(56.7)
 end
