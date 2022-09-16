0aa88d4e86cb016d4e9200bf2ead86380c2231d1
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-17 00:47:59 +0800

    Update PhysicsScalar.jl

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -1,17 +1,16 @@
 using Unitful
 using Test
 
-function (A::Unitful.FreeUnits)(m::Real)
-    return m * A
-end
-function Unitful.dimension(PS::PhysicsScalar)
-    return dimension(PS.magnitude)
-end
-
 abstract type PhysicsScalar <: Real end
     function Base.:+(a::T, b::T) where {T<:PhysicsScalar}
         c = a.magnitude + b.magnitude
         return T(c)
+    end
+    function (A::Unitful.FreeUnits)(m::Real)
+        return m * A
+    end
+    function Unitful.dimension(PS::PhysicsScalar)
+        return dimension(PS.magnitude)
     end
 
     function PhysicsScalar(Q::Quantity)
@@ -50,27 +49,44 @@ abstract type PhysicsScalar <: Real end
         end
     end
 
+    struct Frequency <:PhysicsScalar
+        magnitude::typeof(1.0u"s^-1")
+
+        function Frequency(Q::Quantity)
+            if sign(ustrip(Q)) == -1
+                error("must be positive")
+            else
+                return new(Q)
+            end
+        end
+        function Frequency(m::Real)
+            return new(m * u"s^-1")
+        end
+    end
+@testset "Frequency Functionalities" begin
+    @test isa(Frequency(3u"s^-1"), Frequency)
+    @test Frequency(12.0u"s^-1") == Frequency(1.2e-8u"ns^-1")
+    @test Frequency(12.3) == Frequency(12.3u"s^-1")
+end
+
 @testset "Unitful added Functionalities" begin
     @test 12.3u"m/s" == u"m/s"(12.3)
     @test dimension(Speed(12)) == dimension(12u"m/s")
 end
 
 @testset "PhysicsScalar Functionalities" begin
-
     @test Speed(12.3) + Speed(44.4) == Speed(56.7)
-    @test PhysicsScalar(12u"m/s") == Speed(12.0)
+    @test isa(PhysicsScalar(12u"m/s"), Speed)
 end
 
 @testset "Speed Functionalities" begin
-    @test dimension(12u"kg") == dimension(u"kg")
-    @test Speed(12u"m/s") == Speed(12.0u"m/s")
-    @test Speed(12u"mm/s") == Speed(12u"m/ks")
+    @test isa(Speed(12.0u"m/s"), Speed)
+    @test Speed(12u"mm/s") == Speed(12.0u"m/ks")
     @test Speed(12.3) == Speed(12.3u"m/s")
 end
 
 @testset "Mass Functionalities" begin
-    @test dimension(Mass(12u"kg")) == dimension(u"kg")
-    @test Mass(12u"kg") == Mass(12.0u"kg")
-    @test Mass(12u"g") == Mass(0.012u"kg")
+    @test isa(Mass(12u"kg"), Mass)
+    @test  Mass(12000u"g") == Mass(12.0u"kg")
     @test Mass(12) == Mass(12u"kg")
 end
