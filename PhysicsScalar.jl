a113d6b3a66463f1d2217dff696d1fd278869f29
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-16 23:42:05 +0800

    Update PhysicsScalar.jl

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -5,6 +5,22 @@ function (A::Unitful.FreeUnits)(m::Real)
     return m * A
 end
 
+abstract type PhysicsScalar <: Real end
+
+    struct Speed <: PhysicsScalar
+        magnitude::typeof(1.0u"m/s")
+
+        function Speed(m::Quantity)
+            if sign(ustrip(m)) == -1
+                error("must be positive")
+            else
+                return new(m)
+            end
+        end
+    end
+
 @testset begin
     @test 12.3u"m/s" == u"m/s"(12.3)
+    @test Speed(1u"m/s") == Speed(1.0u"m/s")
+    @test Speed(1u"mm/s") == Speed(1u"m/ks")
 end
