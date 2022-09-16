127b5590ec26f67b6ea184df842eb31849f0096d
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-16 23:46:03 +0800

    Update PhysicsScalar.jl

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -17,10 +17,14 @@ abstract type PhysicsScalar <: Real end
                 return new(m)
             end
         end
+        function Speed(m::Real)
+            return new(m * u"m/s")
+        end
     end
 
 @testset begin
     @test 12.3u"m/s" == u"m/s"(12.3)
-    @test Speed(1u"m/s") == Speed(1.0u"m/s")
-    @test Speed(1u"mm/s") == Speed(1u"m/ks")
+    @test Speed(12u"m/s") == Speed(12.0u"m/s")
+    @test Speed(12u"mm/s") == Speed(12u"m/ks")
+    @test Speed(12.3) == Speed(12.3u"m/s")
 end
