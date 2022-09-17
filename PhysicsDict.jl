7b69e08e7dcb1dd949af29ff26b05d69f52ca287
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-18 01:03:26 +0800

    Update PhysicsDict.jl

--- PhysicsDict.jl
+++ PhysicsDict.jl
@@ -1,4 +1,12 @@
 vector_dict = Dict(
-dimension(u"m/s") => Velocity,
-dimension(u"m") => Position
+    dimension(u"m/s") => Velocity,
+    dimension(u"m") => Position
+)
+
+scalar_dict = Dict(
+    dimension(u"s") => Time,
+    dimension(u"m") => Length,
+    dimension(u"kg") => Mass,
+    dimension(u"m/s") => Speed,
+    dimension(u"s^-1") => Frequency
 )
