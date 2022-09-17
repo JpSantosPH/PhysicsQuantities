aaef674e297c78afdd53b61671996854650442d5
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-18 01:02:27 +0800

    Update PhysicsScalar.jl
    Overhaul of PhysicsScalar. From a number subtype to a vector since I can't make the basic functionality of Base library work with it.

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -1,54 +1,74 @@
 using LinearAlgebra
 using Unitful
 
-abstract type PhysicsScalar <: Real end
-    function Base.:+(a::T, b::T) where {T<:PhysicsScalar}
-        c = a.magnitude + b.magnitude
-        return T(c)
+abstract type PhysicsScalar <: AbstractVector{Number} end
+    Base.size(PS::PhysicsScalar) = (1,)
+    function Base.getindex(PS::PhysicsScalar, i::Int)
+        if i == 1
+            return PS.magnitude
+        end
     end
-    function Base.:*(PS::PhysicsScalar, m::Real)
-        a = PS.magnitude * m
-        return PhysicsScalar(a)
-    end
-    function Base.:*(m::Real, PS::PhysicsScalar)
-        a = PS.magnitude * m
-        return PhysicsScalar(a)
-    end
-
-    function (A::Unitful.FreeUnits)(m::Real)
+    function (A::Unitful.FreeUnits)(m::Number)
         return m * A
     end
     function Unitful.dimension(PS::PhysicsScalar)
         return dimension(PS.magnitude)
     end
 
-    function PhysicsScalar(Q::Quantity)
-        if dimension(Q) == dimension(u"m/s")
-            return Speed(Q)
+    function PhysicsScalar(Q::Quantity; dict::Dict=scalar_dict)
+        if haskey(dict, dimension(Q))
+            return dict[dimension(Q)](Q)
+        else
+            return Q
         end
+    end
+    function PhysicsScalar(VQ::Vector{Quantity})
+        return PhysicsScalar(VQ...)
+    end
+    function Base.:*(n::Number, PS::PhysicsScalar)
+        return PhysicsScalar(n * PS.magnitude)
+    end
+    function Base.:*(PS::PhysicsScalar, n::Number)
+        return PhysicsScalar(PS.magnitude * n)
+    end
+    function Base.:*(V::Vector, PS::PhysicsScalar)
+        return V .* PS.magnitude
+    end
+    function Base.:*(PS::PhysicsScalar, V::Vector)
+        return PS.magnitude .* V
     end
 
     struct Time <: PhysicsScalar
         magnitude::typeof(1.0u"s")
+
+        Time(magnitude::Quantity=0.0u"s") = new(magnitude)
     end
-        Time(m::Real) = Time(m*u"s")
+        Time(m::Number=0.0) = Time(m*u"s")
 
     struct Length <: PhysicsScalar
         magnitude::typeof(1.0u"m")
+
+        Length(magnitude::Quantity=0.0u"m") = new(magnitude)
     end
-        Length(m::Real) = Length(m * u"m")
+        Length(m::Number=0) = Length(m * u"m")
 
     struct Mass <: PhysicsScalar
         magnitude::typeof(1.0u"kg")
+
+        Mass(magnitude::Quantity=0.0u"kg") = new(magnitude)
     end
-        Mass(m::Real) = Mass(m * u"kg")
+        Mass(m::Number=0.0) = Mass(m * u"kg")
 
     struct Speed <: PhysicsScalar
         magnitude::typeof(1.0u"m/s")
+
+        Speed(magnitude::Quantity=0.0u"m/s") = new(magnitude)
     end
-        Speed(m::Real) = Speed(m * u"m/s")
+        Speed(m::Number=0.0) = Speed(m * u"m/s")
 
     struct Frequency <:PhysicsScalar
         magnitude::typeof(1.0u"s^-1")
+
+        Frequency(magnitude::Quantity=0.0u"s^-1") = new(magnitude)
     end
-        Frequency(m::Real) = Frequency(m * u"s^-1")
+        Frequency(m::Number=0.0) = Frequency(m * u"s^-1")
