53f3fb01ac03fbce3ac2f9f04026b64bf0795664
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-17 13:42:00 +0800

    Update PhysicsScalar.jl

--- PhysicsScalar.jl
+++ PhysicsScalar.jl
@@ -26,6 +26,13 @@ abstract type PhysicsScalar <: Real end
         Time(m::Real) = new(m * u"s")
     end
 
+    struct Length <: PhysicsScalar
+        magnitude::typeof(1.0u"m")
+
+        Length(Q::Quantity) = new(Q)
+        Length(m::Real) = new(m * u"m")
+    end
+
     struct Mass <: PhysicsScalar
         magnitude::typeof(1.0u"kg")
 
@@ -47,11 +54,10 @@ abstract type PhysicsScalar <: Real end
         Frequency(m::Real) = new(m * u"s^-1")
     end
 
-    
-@testset "Frequency Functionalities" begin
-    @test isa(Frequency(3u"s^-1"), Frequency)
-    @test Frequency(12.0u"s^-1") == Frequency(1.2e-8u"ns^-1")
-    @test Frequency(12.3) == Frequency(12.3u"s^-1")
+
+@testset "PhysicsScalar Functionalities" begin
+    @test Speed(12.3) + Speed(44.4) == Speed(56.7)
+    @test isa(PhysicsScalar(12u"m/s"), Speed)
 end
 
 @testset "Unitful added Functionalities" begin
@@ -59,9 +65,22 @@ end
     @test dimension(Speed(12)) == dimension(12u"m/s")
 end
 
-@testset "PhysicsScalar Functionalities" begin
-    @test Speed(12.3) + Speed(44.4) == Speed(56.7)
-    @test isa(PhysicsScalar(12u"m/s"), Speed)
+@testset "Time Functionalities" begin
+    @test isa(Time(12u"s"), Time)
+    @test Time(12u"ms") == Time(0.012u"s")
+    @test Time(12) == Time(12.0u"s")
+end
+
+@testset "Length Functionalities" begin
+    @test isa(Length(12u"m"), Length)
+    @test Length(12u"mm") == Length(0.012u"m")
+    @test Length(12) == Length(12.0u"m")
+end
+
+@testset "Mass Functionalities" begin
+    @test isa(Mass(12u"kg"), Mass)
+    @test  Mass(12000u"g") == Mass(12.0u"kg")
+    @test Mass(12) == Mass(12u"kg")
 end
 
 @testset "Speed Functionalities" begin
@@ -70,20 +89,8 @@ end
     @test Speed(12.3) == Speed(12.3u"m/s")
 end
 
-@testset "Mass Functionalities" begin
-    @test isa(Mass(12u"kg"), Mass)
-    @test  Mass(12000u"g") == Mass(12.0u"kg")
-    @test Mass(12) == Mass(12u"kg")
-end
-
-@testset "Mass Functionalities" begin
-    @test isa(Mass(12u"kg"), Mass)
-    @test  Mass(12000u"g") == Mass(12.0u"kg")
-    @test Mass(12) == Mass(12u"kg")
-end
-
-@testset "Time Functionalities" begin
-    @test isa(Time(12u"s"), Time)
-    @test Time(12u"ms") == Time(0.012u"s")
-    @test Time(12) == Time(12.0u"s")
+@testset "Frequency Functionalities" begin
+    @test isa(Frequency(3u"s^-1"), Frequency)
+    @test Frequency(12.0u"s^-1") == Frequency(1.2e-8u"ns^-1")
+    @test Frequency(12.3) == Frequency(12.3u"s^-1")
 end
