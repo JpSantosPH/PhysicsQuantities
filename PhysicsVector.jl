2042e5c912b9e673a3f129b2d66486a5c7a886b0
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-17 16:24:58 +0800

    Update PhysicsVector.jl

--- PhysicsVector.jl
+++ PhysicsVector.jl
@@ -1,5 +1,7 @@
 using LinearAlgebra
+using Unitful
 using Test
+using BenchmarkTools
 
 struct BasisVectors <: AbstractVector{Vector}
     e₁::CartesianCoordinate
@@ -7,9 +9,7 @@ struct BasisVectors <: AbstractVector{Vector}
     e₃::CartesianCoordinate
 end
     function Base.size(BV::BasisVectors)
-        if length(BV.e₁) == length(BV.e₂) == length(BV.e₃)
-            return size(BV.e₁)
-        end
+        return (3,)
     end
     function Base.getindex(BV::BasisVectors, i::Int)
         if i == 1
@@ -29,7 +29,21 @@ end
 
 abstract type PhysicsVector <: AbstractVector{Real} end
 
-@testset "PhysicsVector Functionalities" begin
+    struct Velocity <: PhysicsVector
+        a₁::typeof(1.0u"m/s")
+        a₂::typeof(1.0u"m/s")
+        a₃::typeof(1.0u"m/s")
+        BV::BasisVectors
+    end
+        function Base.size(Velo::Velocity)
+            return (3,)
+        end
+        function Base.getindex(Velo::Velocity, i::Int)
+            V = Velo.a₁*Velo.BV.e₁ + Velo.a₂*Velo.BV.e₂ + Velo.a₃*Velo.BV.e₃
+            return V[i]
+        end
+
+@testset "BasisVectors Functionalities" begin
     @test let
         a = CartesianCoordinate(1, 2, 3)
         b = CartesianCoordinate(4, 5, 6)
@@ -44,4 +58,8 @@ abstract type PhysicsVector <: AbstractVector{Real} end
         c = CartesianCoordinate(0, 0, 1)
         BV.e₁ == a && BV.e₂ == b && BV.e₃ == c
     end
+end
+
+@testset "Velocity Functionalities" begin
+    @test isa(Velocity(1÷1 * u"m/s", 2//1 * u"m/s", 3/1 * u"m/s", BasisVectors()), Velocity)
 end
