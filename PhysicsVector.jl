b1b49b6aea4beb3d5e8ae1b4fc352b388f755918
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-21 22:51:57 +0800

    Update PhysicsVector.jl

--- PhysicsVector.jl
+++ PhysicsVector.jl
@@ -146,3 +146,13 @@ abstract type PhysicsVector <: AbstractVector{Number} end
         y::typeof(1.0u"N/C")
         z::typeof(1.0u"N/C")
     end
+        function ElectricField(q::Charge)
+            E(r::Position) = (1/(4π*(8.8541878128e-12u"F*m^-1"))) * (q/r^2) * unitvec(r)
+            E(r::Length) = uconvert(u"N/C", (1/(4π*(8.8541878128e-12u"F*m^-1"))) * (q/r^2))
+            return E
+        end
+        function ElectricField(q::Quantity)
+            if dimension(q) == dimension(u"C")
+                return ElectricField(Charge(q))
+            end
+        end
