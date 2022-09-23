e3f205daf3bcaa525cce703af663cccfa23489fb
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-23 18:09:29 +0800

    Update optimization.jl

--- optimization.jl
+++ optimization.jl
@@ -1,10 +1,22 @@
-
 using BenchmarkTools
+
+@benchmark [[1u"m/s^2", 2u"m/s^2", 3u"m/s^2"] .* i*u"s" for i in 1:1000]
+@benchmark [(1u"m")/(i*u"s") for i in 1:10000]
+@benchmark [(1u"m")/((i*u"s")^2) for i in 1:10000]
+@benchmark [1/2 * 10u"kg" * (i*u"m/s")^2 for i in 1:10000]
+@benchmark [my_electric_field(4u"nC")(i*u"m") for i in 1:1000]
+@benchmark [my_electric_field(4u"nC")([i*u"m", 0u"m", 0u"m"]) for i in 1:1000]
+
+
 
 @benchmark [Physics(Acceleration(1,2,3) * Time(i)) for i in 1:1000]
 @benchmark [Acceleration(1,2,3) * Time(i) for i in 1:1000]
-@benchmark [[1u"m/s^2", 2u"m/s^2", 3u"m/s^2"] .* i*u"s" for i in 1:1000]
-
+@benchmark [Physics(Length(1)/Time(i)) for i in 1:10000]
+@benchmark [Length(1)/Time(i) for i in 1:10000]
 @benchmark [Physics(Length(1)/Time(i)^2) for i in 1:10000]
-@benchmark [(1u"m")/((i*u"s")^2) for i in 1:10000]
-
+@benchmark [kinetic_energy(Mass(10))(Speed(i)) for i in 1:10000]
+@benchmark [kinetic_energy(Mass(10))(Velocity(i)) for i in 1:10000]
+@benchmark [Physics(kinetic_energy(Mass(10))(Speed(i))) for i in 1:10000]
+@benchmark [Physics(kinetic_energy(Mass(10))(Velocity(i))) for i in 1:10000]
+@benchmark [ElectricField(4u"nC")(Length(i)) for i in 1:1000]
+@benchmark [ElectricField(4u"nC")(Position(i)) for i in 1:1000]
