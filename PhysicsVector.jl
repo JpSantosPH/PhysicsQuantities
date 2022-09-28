896282918f543983dea239c3c7a5076f678744d1
JpSantosPH <santosjohnpaul1111@gmail.com>
2022-09-29 00:48:17 +0800

    Overhaul of the typing system of PhysicsVector and PhysicsScalar
    I did it!! Overhaul of the typing system of PhysicsVector and PhysicsScalar to be able to preserve SI prefixes instead of immediately converting it to decimal points which is prone to floating point error. It's optimized this time. FIxed broadcasting between PhysicsVector and PhysicsScalar

--- PhysicsVector.jl
+++ PhysicsVector.jl
@@ -4,280 +4,221 @@ using Unitful
 abstract type PhysicsVector <: AbstractVector{Number} end
     Base.size(PV::PhysicsVector) = (3,)
     function Base.getindex(PV::PhysicsVector, i::Int)
-        if i == 1
-            return PV.x
-        elseif i == 2
-            return PV.y
-        elseif i == 3
-            return PV.z
+        if     i == 1; return PV.x
+        elseif i == 2; return PV.y
+        elseif i == 3; return PV.z
         end
     end
     Base.similar(PV::PhysicsVector) = PhysicsVector(PV.x, PV.y, PV.z)
     function Base.setindex!(PV::PhysicsVector, val, i::Int)
-        if i == 1
-            return PhysicsVector(val, PV.y, PV.x)
-        elseif i == 2
-            return PhysicsVector(PV.x, val, PV.z)
-        elseif i == 3
-            return PhysicsVector(PV.x, PV.y, val)
+        val = ps_strip(val)
+        if     i == 1; return PhysicsVector(val,  PV.y, PV.z)
+        elseif i == 2; return PhysicsVector(PV.x, val,  PV.z)
+        elseif i == 3; return PhysicsVector(PV.x, PV.y, val)
         end
     end
-
-    struct CartesianCoordinate <: PhysicsVector
-        x::AbstractFloat
-        y::AbstractFloat
-        z::AbstractFloat
-
-        function CartesianCoordinate(x::Number=0.0, y::Number=0.0, z::Number=0.0)
-            if !(x isa AbstractFloat)
-                x = convert(Float64, x)
-            end
-            if !(y isa AbstractFloat)
-                y = convert(Float64, y)
-            end
-            if !(z isa AbstractFloat)
-                z = convert(Float64, z)
-            end
-            return new(x, y, z)
-        end
-    end
-        CartesianCoordinate(args) = CartesianCoordinate(args...)
 
     struct GeneralVector{T<:Number} <: PhysicsVector
         x::T
         y::T
         z::T
     end
-        Base.showarg(io::IO, GV::GeneralVector, toplevel) = print(io, :GeneralVector)
+        function GeneralVector(x::Number, y::Number, z::Number)
+            x, y, z = promote(x, y, z)
+            return GeneralVector(x, y, z)
+        end
+        Base.showarg(io::IO, V::GeneralVector, toplevel) = print(io, GeneralVector)
         GeneralVector( (x, y, z) ) = GeneralVector(x, y, z)
         GeneralVector() = GeneralVector(0.0, 0.0, 0.0)
-    
-### Named units derived from SI base units ###
-    struct Position <: PhysicsVector
-        x::typeof(1.0u"m")
-        y::typeof(1.0u"m")
-        z::typeof(1.0u"m")
 
-        function Position(x::Number=0.0u"m", y::Number=0.0u"m", z::Number=0.0u"m")
-            if !(x isa Quantity)
-                x = x * u"m"
-            end
-            if !(y isa Quantity)
-                y = y * u"m"
-            end
-            if !(z isa Quantity)
-                z = z * u"m"
-            end
-            return new(x, y, z)
-        end
+    struct CartesianCoordinate{T<:Float64} <: PhysicsVector
+        x::T
+        y::T
+        z::T
     end
+        function CartesianCoordinate(x::Number=0.0, y::Number=0.0, z::Number=0.0)
+            x, y, z = convert.(Float64, (x, y, z))
+            return CartesianCoordinate(x, y, z)
+        end
+        CartesianCoordinate(args) = CartesianCoordinate(args...)
+        Base.showarg(io::IO, V::CartesianCoordinate, toplevel) = print(io, CartesianCoordinate)
+
+    struct Position{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"m"), Unitful.FreeUnits{A, dimension(u"m"), nothing}}
+        y::Quantity{Float64, dimension(u"m"), Unitful.FreeUnits{B, dimension(u"m"), nothing}}
+        z::Quantity{Float64, dimension(u"m"), Unitful.FreeUnits{C, dimension(u"m"), nothing}}
+    end
+        function Position(x::Number=0.0u"m", y::Number=0.0u"m", z::Number=0.0u"m")
+            if !(x isa Quantity); x = x*u"m" end
+            if !(y isa Quantity); y = y*u"m" end
+            if !(z isa Quantity); z = z*u"m" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"m")}, (x, y, z))
+            return Position(x, y, z)
+        end
+        Base.showarg(io::IO, V::Position, toplevel) = print(io, Position)
         Position(args) = Position(args...)
 
-    struct Force <: PhysicsVector
-        x::typeof(1.0u"N")
-        y::typeof(1.0u"N")
-        z::typeof(1.0u"N")
-
-        function Force(x::Number=0.0u"N", y::Number=0.0u"N", z::Number=0.0u"N")
-            if !(x isa Quantity)
-                x = x * u"N"
-            end
-            if !(y isa Quantity)
-                y = y * u"N"
-            end
-            if !(z isa Quantity)
-                z = z * u"N"
-            end
-            return new(x, y, z)
-        end
+    struct Force{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"N"), Unitful.FreeUnits{A, dimension(u"N"), nothing}}
+        y::Quantity{Float64, dimension(u"N"), Unitful.FreeUnits{B, dimension(u"N"), nothing}}
+        z::Quantity{Float64, dimension(u"N"), Unitful.FreeUnits{C, dimension(u"N"), nothing}}
     end
+        function Force(x::Number=0.0u"N", y::Number=0.0u"N", z::Number=0.0u"N")
+            if !(x isa Quantity); x = x*u"N" end
+            if !(y isa Quantity); y = y*u"N" end
+            if !(z isa Quantity); z = z*u"N" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"N")}, (x, y, z))
+            return Force(x, y, z)
+        end
+        Base.showarg(io::IO, V::Force, toplevel) = print(io, Force)
         Force(args) = Force(args...)
 
-### Kinematic SI derived units ###
-    struct Velocity <: PhysicsVector
-        x::typeof(1.0u"m/s")
-        y::typeof(1.0u"m/s")
-        z::typeof(1.0u"m/s")
-
-        function Velocity(x::Number=0.0u"m/s", y::Number=0.0u"m/s", z::Number=0.0u"m/s")
-            if !isa(x, Quantity)
-                x = x * u"m/s"
-            end
-            if !isa(y, Quantity)
-                y = y * u"m/s"
-            end
-            if !isa(z, Quantity)
-                z = z * u"m/s"
-            end
-            return new(x, y, z)
-        end
+    struct Velocity{A,B,C}  <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"m/s"), Unitful.FreeUnits{A, dimension(u"m/s"), nothing}}
+        y::Quantity{Float64, dimension(u"m/s"), Unitful.FreeUnits{B, dimension(u"m/s"), nothing}}
+        z::Quantity{Float64, dimension(u"m/s"), Unitful.FreeUnits{C, dimension(u"m/s"), nothing}}
     end
+        function Velocity(x::Number=0.0u"m/s", y::Number=0.0u"m/s", z::Number=0.0u"m/s")
+            if !(x isa Quantity); x = x*u"m/s" end
+            if !(y isa Quantity); y = y*u"m/s" end
+            if !(z isa Quantity); z = z*u"m/s" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"m/s")}, (x, y, z))
+            return Velocity(x, y, z)
+        end
+        Base.showarg(io::IO, V::Velocity, toplevel) = print(io, Velocity)
         Velocity(args) = Velocity(args...)
 
-    struct Acceleration <: PhysicsVector
-        x::typeof(1.0u"m/s^2")
-        y::typeof(1.0u"m/s^2")
-        z::typeof(1.0u"m/s^2")
-
-        function Acceleration(x::Number=0.0u"m/s^2", y::Number=0.0u"m/s^2", z::Number=0.0u"m/s^2")
-            if !isa(x, Quantity)
-                x = x * u"m/s^2"
-            end
-            if !isa(y, Quantity)
-                y = y * u"m/s^2"
-            end
-            if !isa(z, Quantity)
-                z = z * u"m/s^2"
-            end
-            return new(x, y, z)
-        end
+    struct Acceleration{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"m/s^2"), Unitful.FreeUnits{A, dimension(u"m/s^2"), nothing}}
+        y::Quantity{Float64, dimension(u"m/s^2"), Unitful.FreeUnits{B, dimension(u"m/s^2"), nothing}}
+        z::Quantity{Float64, dimension(u"m/s^2"), Unitful.FreeUnits{C, dimension(u"m/s^2"), nothing}}
     end
+        function Acceleration(x::Number=0.0u"m/s^2", y::Number=0.0u"m/s^2", z::Number=0.0u"m/s^2")
+            if !(x isa Quantity); x = x*u"m/s^2" end
+            if !(y isa Quantity); y = y*u"m/s^2" end
+            if !(z isa Quantity); z = z*u"m/s^2" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"m/s^2")}, (x, y, z))
+            return Acceleration(x, y, z)
+        end
+        Base.showarg(io::IO, V::Acceleration, toplevel) = print(io, Acceleration)
         Acceleration(args) = Acceleration(args...)
     
-    struct Jerk <: PhysicsVector
-    x::typeof(1.0u"m/s^3")
-    y::typeof(1.0u"m/s^3")
-    z::typeof(1.0u"m/s^3")
-
-    function Jerk(x::Number=0.0u"m/s^3", y::Number=0.0u"m/s^3", z::Number=0.0u"m/s^3")
-        if !isa(x, Quantity)
-            x = x * u"m/s^3"
-        end
-        if !isa(y, Quantity)
-            y = y * u"m/s^3"
-        end
-        if !isa(z, Quantity)
-            z = z * u"m/s^3"
-        end
-        return new(x, y, z)
+    struct Jerk{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"m/s^3"), Unitful.FreeUnits{A, dimension(u"m/s^3"), nothing}}
+        y::Quantity{Float64, dimension(u"m/s^3"), Unitful.FreeUnits{B, dimension(u"m/s^3"), nothing}}
+        z::Quantity{Float64, dimension(u"m/s^3"), Unitful.FreeUnits{C, dimension(u"m/s^3"), nothing}}
     end
-end
-    Jerk(args) = Jerk(args...)
+        function Jerk(x::Number=0.0u"m/s^3", y::Number=0.0u"m/s^3", z::Number=0.0u"m/s^3")
+            if !(x isa Quantity); x = x*u"m/s^3" end
+            if !(y isa Quantity); y = y*u"m/s^3" end
+            if !(z isa Quantity); z = z*u"m/s^3" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"m/s^3")}, (x, y, z))
+            return Jerk(x, y, z)
+        end
+        Base.showarg(io::IO, V::Jerk, toplevel) = print(io, Jerk)
+        Jerk(args) = Jerk(args...)
 
-    struct Snap <: PhysicsVector
-        x::typeof(1.0u"m/s^4")
-        y::typeof(1.0u"m/s^4")
-        z::typeof(1.0u"m/s^4")
-    
+    struct Snap{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"m/s^4"), Unitful.FreeUnits{A, dimension(u"m/s^4"), nothing}}
+        y::Quantity{Float64, dimension(u"m/s^4"), Unitful.FreeUnits{B, dimension(u"m/s^4"), nothing}}
+        z::Quantity{Float64, dimension(u"m/s^4"), Unitful.FreeUnits{C, dimension(u"m/s^4"), nothing}}
+    end
         function Snap(x::Number=0.0u"m/s^4", y::Number=0.0u"m/s^4", z::Number=0.0u"m/s^4")
-            if !isa(x, Quantity)
-                x = x * u"m/s^4"
-            end
-            if !isa(y, Quantity)
-                y = y * u"m/s^4"
-            end
-            if !isa(z, Quantity)
-                z = z * u"m/s^4"
-            end
-            return new(x, y, z)
+            if !(x isa Quantity); x = x*u"m/s^4" end
+            if !(y isa Quantity); y = y*u"m/s^4" end
+            if !(z isa Quantity); z = z*u"m/s^4" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"m/s^4")}, (x, y, z))
+            return Snap(x, y, z)
         end
-    end
+        Base.showarg(io::IO, V::Snap, toplevel) = print(io, Snap)
         Snap(args) = Snap(args...)
 
-    struct AngularVelocity <: PhysicsVector
-        x::typeof(1.0u"rad/s")
-        y::typeof(1.0u"rad/s")
-        z::typeof(1.0u"rad/s")
-
-        function AngularVelocity(x::Number=0.0u"rad/s", y::Number=0.0u"rad/s", z::Number=0.0u"rad/s")
-            if !isa(x, Quantity)
-                x = x * u"rad/s"
-            end
-            if !isa(y, Quantity)
-                y = y * u"rad/s"
-            end
-            if !isa(z, Quantity)
-                z = z * u"rad/s"
-            end
-            return new(x, y, z)
-        end
+    struct AngularVelocity{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"rad/s"), Unitful.FreeUnits{A, dimension(u"rad/s"), nothing}}
+        y::Quantity{Float64, dimension(u"rad/s"), Unitful.FreeUnits{B, dimension(u"rad/s"), nothing}}
+        z::Quantity{Float64, dimension(u"rad/s"), Unitful.FreeUnits{C, dimension(u"rad/s"), nothing}}
     end
+        function AngularVelocity(x::Number=0.0u"rad/s", y::Number=0.0u"rad/s", z::Number=0.0u"rad/s")
+            if !(x isa Quantity); x = x*u"rad/s" end
+            if !(y isa Quantity); y = y*u"rad/s" end
+            if !(z isa Quantity); z = z*u"rad/s" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"rad/s")}, (x, y, z))
+            return AngularVelocity(x, y, z)
+        end
+        Base.showarg(io::IO, V::AngularVelocity, toplevel) = print(io, AngularVelocity)
         AngularVelocity(args) = AngularVelocity(args...)
 
-    struct AngularAcceleration <: PhysicsVector
-        x::typeof(1.0u"rad/s^2")
-        y::typeof(1.0u"rad/s^2")
-        z::typeof(1.0u"rad/s^2")
-
-        function AngularAcceleration(x::Number=0.0u"rad/s^2", y::Number=0.0u"rad/s^2", z::Number=0.0u"rad/s^2")
-            if !isa(x, Quantity)
-                x = x * u"rad/s^2"
-            end
-            if !isa(y, Quantity)
-                y = y * u"rad/s^2"
-            end
-            if !isa(z, Quantity)
-                z = z * u"rad/s^2"
-            end
-            return new(x, y, z)
-        end
+    struct AngularAcceleration{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"rad/s^2"), Unitful.FreeUnits{A, dimension(u"rad/s^2"), nothing}}
+        y::Quantity{Float64, dimension(u"rad/s^2"), Unitful.FreeUnits{B, dimension(u"rad/s^2"), nothing}}
+        z::Quantity{Float64, dimension(u"rad/s^2"), Unitful.FreeUnits{C, dimension(u"rad/s^2"), nothing}}
     end
+        function AngularAcceleration(x::Number=0.0u"rad/s^2", y::Number=0.0u"rad/s^2", z::Number=0.0u"rad/s^2")
+            if !(x isa Quantity); x = x*u"rad/s^2" end
+            if !(y isa Quantity); y = y*u"rad/s^2" end
+            if !(z isa Quantity); z = z*u"rad/s^2" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"rad/s^2")}, (x, y, z))
+            return AngularAcceleration(x, y, z)
+        end
+        Base.showarg(io::IO, V::AngularAcceleration, toplevel) = print(io, AngularAcceleration)
         AngularAcceleration(args) = AngularAcceleration(args...)
 
-    struct Momentum <: PhysicsVector
-        x::typeof(1.0u"kg*m/s")
-        y::typeof(1.0u"kg*m/s")
-        z::typeof(1.0u"kg*m/s")
-
-        function Momentum(x::Number=0.0u"kg*m/s", y::Number=0.0u"kg*m/s", z::Number=0.0u"kg*m/s")
-            if !isa(x, Quantity)
-                x = x * u"kg*m/s"
-            end
-            if !isa(y, Quantity)
-                y = y * u"kg*m/s"
-            end
-            if !isa(z, Quantity)
-                z = z * u"kg*m/s"
-            end
-            return new(x, y, z)
-        end
+    struct Momentum{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"kg*m/s"), Unitful.FreeUnits{A, dimension(u"kg*m/s"), nothing}}
+        y::Quantity{Float64, dimension(u"kg*m/s"), Unitful.FreeUnits{B, dimension(u"kg*m/s"), nothing}}
+        z::Quantity{Float64, dimension(u"kg*m/s"), Unitful.FreeUnits{C, dimension(u"kg*m/s"), nothing}}
     end
+        function Momentum(x::Number=0.0u"kg*m/s", y::Number=0.0u"kg*m/s", z::Number=0.0u"kg*m/s")
+            if !(x isa Quantity); x = x*u"kg*m/s" end
+            if !(y isa Quantity); y = y*u"kg*m/s" end
+            if !(z isa Quantity); z = z*u"kg*m/s" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"kg*m/s")}, (x, y, z))
+            return Momentum(x, y, z)
+        end
+        Base.showarg(io::IO, V::Momentum, toplevel) = print(io, Momentum)
         Momentum(args) = Momentum(args...)
         
-    struct AngularMomentum <: PhysicsVector
-        x::typeof(1.0u"N*m*s")
-        y::typeof(1.0u"N*m*s")
-        z::typeof(1.0u"N*m*s")
-
-        function AngularMomentum(x::Number=0.0u"N*m*s", y::Number=0.0u"N*m*s", z::Number=0.0u"N*m*s")
-            if !isa(x, Quantity)
-                x = x * u"N*m*s"
-            end
-            if !isa(y, Quantity)
-                y = y * u"N*m*s"
-            end
-            if !isa(z, Quantity)
-                z = z * u"N*m*s"
-            end
-            return new(x, y, z)
-        end
+    struct AngularMomentum{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"N*m*s"), Unitful.FreeUnits{A, dimension(u"N*m*s"), nothing}}
+        y::Quantity{Float64, dimension(u"N*m*s"), Unitful.FreeUnits{B, dimension(u"N*m*s"), nothing}}
+        z::Quantity{Float64, dimension(u"N*m*s"), Unitful.FreeUnits{C, dimension(u"N*m*s"), nothing}}
     end
+        function AngularMomentum(x::Number=0.0u"N*m*s", y::Number=0.0u"N*m*s", z::Number=0.0u"N*m*s")
+            if !(x isa Quantity); x = x*u"N*m*s" end
+            if !(y isa Quantity); y = y*u"N*m*s" end
+            if !(z isa Quantity); z = z*u"N*m*s" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"N*m*s")}, (x, y, z))
+            return AngularMomentum(x, y, z)
+        end
+        Base.showarg(io::IO, V::AngularMomentum, toplevel) = print(io, AngularMomentum)
         AngularMomentum(args) = AngularMomentum(args...)
 
-    struct Torque <: PhysicsVector
-        x::typeof(1.0u"N*m")
-        y::typeof(1.0u"N*m")
-        z::typeof(1.0u"N*m")
-
-        function Torque(x::Number=0.0u"N*m", y::Number=0.0u"N*m", z::Number=0.0u"N*m")
-            if !isa(x, Quantity)
-                x = x * u"N*m"
-            end
-            if !isa(y, Quantity)
-                y = y * u"N*m"
-            end
-            if !isa(z, Quantity)
-                z = z * u"N*m"
-            end
-            return new(x, y, z)
-        end
+    struct Torque{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"N*m"), Unitful.FreeUnits{A, dimension(u"N*m"), nothing}}
+        y::Quantity{Float64, dimension(u"N*m"), Unitful.FreeUnits{B, dimension(u"N*m"), nothing}}
+        z::Quantity{Float64, dimension(u"N*m"), Unitful.FreeUnits{C, dimension(u"N*m"), nothing}}
     end
+        function Torque(x::Number=0.0u"N*m", y::Number=0.0u"N*m", z::Number=0.0u"N*m")
+            if !(x isa Quantity); x = x*u"N*m" end
+            if !(y isa Quantity); y = y*u"N*m" end
+            if !(z isa Quantity); z = z*u"N*m" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"N*m")}, (x, y, z))
+            return Torque(x, y, z)
+        end
+        Base.showarg(io::IO, V::Torque, toplevel) = print(io, Torque)
         Torque(args) = Torque(args...)
 
-    struct ElectricFieldStrength <: PhysicsVector
-        x::typeof(1.0u"N/C")
-        y::typeof(1.0u"N/C")
-        z::typeof(1.0u"N/C")
+    struct ElectricFieldStrength{A,B,C} <: PhysicsVector where {A,B,C}
+        x::Quantity{Float64, dimension(u"N/C"), Unitful.FreeUnits{A, dimension(u"N/C"), nothing}}
+        y::Quantity{Float64, dimension(u"N/C"), Unitful.FreeUnits{B, dimension(u"N/C"), nothing}}
+        z::Quantity{Float64, dimension(u"N/C"), Unitful.FreeUnits{C, dimension(u"N/C"), nothing}}
     end
-        ElectricFieldStrength( (x, y, z) ) = ElectricFieldStrength(x, y, z)
-    
+        function ElectricFieldStrength(x::Number=0.0u"N/C", y::Number=0.0u"N/C", z::Number=0.0u"N/C")
+            if !(x isa Quantity); x = x*u"N/C" end
+            if !(y isa Quantity); y = y*u"N/C" end
+            if !(z isa Quantity); z = z*u"N/C" end
+            x, y, z = convert.(Quantity{Float64, dimension(u"N/C")}, (x, y, z))
+            return ElectricFieldStrength(x, y, z)
+        end
+        Base.showarg(io::IO, V::ElectricFieldStrength, toplevel) = print(io, ElectricFieldStrength)
+        ElectricFieldStrength(args) = ElectricFieldStrength(args...)
